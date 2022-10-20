2bd5dec489b78ee3f3b4b5aaff5ff8f7c1d40262
rameshkothamasu <115606141+rameshkothamasu@users.noreply.github.com>
2022-10-20 14:04:50 -0400

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -2,4 +2,7 @@
 echo "Pipeline is running"
 ./gradlew test
 ./gradlew assemble
-docker build -t my-app .
+IMAGE_NAME="${DOCKER_HUB_USER_NAME}/my-app:${BUILD_NUMBER}"
+docker build -t $IMAGE_NAME .
+docker login -u "${DOCKER_HUB_USER_NAME}" -p "${DOCKER_HUB_USER_PASSWORD}"
+docker push $IMAGE_NAME
