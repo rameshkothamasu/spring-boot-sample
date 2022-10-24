4514b32cb6c4f916c4f09308d6f6f778772b40a9
rameshkothamasu <115606141+rameshkothamasu@users.noreply.github.com>
2022-10-24 13:12:00 -0400

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Pipeline is running"
 ./gradlew test
+./gradlew build
 ./gradlew assemble
 IMAGE_NAME="${DOCKER_HUB_USER_NAME}/my-app:${BUILD_NUMBER}"
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME . --build-arg JAR_FILE=./build/libs/spring-boot-api-example-0.1.0-SNAPSHOT.jar
 docker login -u "${DOCKER_HUB_USER_NAME}" -p "${DOCKER_HUB_USER_PASSWORD}"
 docker push $IMAGE_NAME
